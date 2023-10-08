56f5e73bab4e30d39346af4379bf14e52942bd54
Masaa <davidmachariamj@gmail.com>
2023-10-08 11:31:07 +0300

    logger

--- brighterMondayScraper.js
+++ brighterMondayScraper.js
@@ -1,29 +1,36 @@
 const puppeteer = require("puppeteer")
 const fs = require("fs")
-
-
+const pino = require("pino")
+const logger = pino({
+    transport: {
+      target: 'pino-pretty'
+    },
+  })
 async function scrape(){
     const searchParam = process.argv[2]
     
     const browser = await puppeteer.launch({headless:"new"})
     const page = await browser.newPage()
+    try {
+        
     
     await page.goto("https://www.brightermonday.co.ke/jobs",{timeout:60000})
 
     await page.waitForSelector("#onetrust-accept-btn-handler")
     await  page.click("#onetrust-accept-btn-handler")
-   
+    logger.info("cookies accepted")
    
     page.waitForNavigation()
     await page.type("#side-search", searchParam, {delay:300})
+    logger.info("search parameters added")
 
-    console.log("hello1")
     await Promise.all([
         page.waitForNavigation(),
         page.click("#job-search-btn")
+    
     ])
 
- 
+    logger.info("loading jobs...")
     const titles = await page.$$eval("div.flex.items-center > a > p", titles=>titles.map(p=>p.innerText))
     const companies = await page.$$eval("div.w-full > p", companies=>companies.map(p=>p.innerText))
     const locations = await page.$$eval("div.flex.flex-wrap > span", locations=>locations.map(span=>span.innerText))
@@ -32,12 +39,16 @@ async function scrape(){
 
     try{
         fs.writeFileSync(`data/${searchParam}.json`, JSON.stringify({jobs:data}), "utf-8")
-       
+       logger.info("jobs successfully loaded")
+       logger.info(data.length +" jobs found")
     }catch(err){
-        console.error(err.message())
+        logger.error("jobs failed to load: " + err.message())
     }
 
     await browser.close()
+} catch (error) {
+       logger.error("something went wrong..  "+ error.message()) 
+}
 }
 
 function processData(titles,companies,locations){
